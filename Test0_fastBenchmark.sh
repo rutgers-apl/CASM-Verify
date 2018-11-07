94ddee2651bc20252b38006f7958a3828eab97bc
Jay Lim <JayLim@nbp-25-123-92.nbp.ruw.rutgers.edu>
2018-11-07 00:21:32 -0500

    Fixed Test0_fastBenchmark.sh

--- Test0_fastBenchmark.sh
+++ Test0_fastBenchmark.sh
@@ -1,18 +1,17 @@
-#ChaCha20
-echo "BENCHMARK: Verifying ChaCha20 implementation"
+#2 rounds of SHA256
+echo "BENCHMARK: Verifying 2 rounds of SHA256"
 echo "Expected result: Equivalent (Time: )"
 python3 main.py --pre test/sha2rnd/pre --post test/sha2rnd/post --p1 test/sha2rnd/dsl --p1lang dsl --p2 test/sha2rnd/asm --p2lang asm  --verif_mode hybrid
-
 
 #ChaCha20
 echo "BENCHMARK: Verifying ChaCha20 implementation"
 echo "Expected result: Equivalent (Time: )"
 python3 main.py --pre test/ChaCha20/pre --post test/ChaCha20/post --p1 test/ChaCha20/dsl --p1lang dsl --p2 test/ChaCha20/asm --p2lang asm  --verif_mode hybrid
 
-#Part of AES128 key expansion for decryption algorithm
-echo "BENCHMARK: Verifying AES key expansion permutation"
+#A small part of AES128 key expansion for decryption algorithm
+echo "BENCHMARK: Verifying permutation algorithm in AES key expansion"
 echo "Expected result: Equivalent (Time: )"
-python3 main.py --pre test/AES_dec_key_permute/pre --post test/AES_dec_key_permute/post --p1 test/AES_dec_key_permute/dsl --p1lang dsl --p2 test/AES_dec_key_permute/asm --p2lang asm  --verif_mode hybrid
+python3 main.py --pre test/AES_dec_key_permute/pre --post test/AES_dec_key_permute/post --p1 test/AES_dec_key_permute/dsl --p1lang dsl --p2 test/AES_dec_key_permute/asm --p2lang asm  --verif_mode hybrid --mem_model 8
 
 #AES_decrypt
 echo "BENCHMARK: Verifying AES128 decryption using 8-bit memory"
